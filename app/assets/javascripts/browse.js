e2f8bdb62fe75e8a2ad8203abec5aeea6ab28efb
Tom MacWright <tom@macwright.org>
2013-06-10 11:20:29 -0700

    Add leaflet-locate control, invisible currently.

--- app/assets/javascripts/browse.js
+++ app/assets/javascripts/browse.js
@@ -60,7 +60,7 @@ $(document).ready(function () {
       return remoteEditHandler(bbox);
     });
 
-    updatelinks(params.lon, params.lat, 16, null, 
+    updatelinks(params.lon, params.lat, 16, null,
                 bbox.getWestLng(), bbox.getSouthLat(),
                 bbox.getEastLng(), bbox.getNorthLat(),
                 object);
